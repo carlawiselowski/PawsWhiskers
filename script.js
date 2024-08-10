a09210c3825f2e38dc8113b94a0157cfe3499e08
carlitia <105894626+carlawiselowski@users.noreply.github.com>
2024-08-09 23:52:48 -0300

    adjust the shadow details

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 
+
+//evento de zoom 
 const items = document.querySelectorAll('.box');
 
 // Função para aumentar o quadro
 function enlargeItem(event) {
     event.currentTarget.style.transform = 'scale(1.1)';
-    event.currentTarget.style.boxShadow = '0 8px 16px rgba(0, 0, 0, 0.2)';
+    event.currentTarget.style.boxShadow = '0 8px 8px rgba(0, 0, 0, 0.2)';
 }
 
 // Função para restaurar o tamanho original
 function resetItem(event) {
     event.currentTarget.style.transform = 'scale(1)';
-    event.currentTarget.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
+    event.currentTarget.style.boxShadow = '4px 2px 8px rgba(0, 0, 0, 0.463)';
 }
 
 // Adiciona os event listeners para cada item
