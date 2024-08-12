3d0f03e686e245489023473f87a3dddecb1dc3d6
carlitia <105894626+carlawiselowski@users.noreply.github.com>
2024-08-11 23:08:48 -0300

    switch the layout to a tab interface

--- script.js
+++ script.js
@@ -20,3 +20,28 @@ items.forEach(item => {
     item.addEventListener('mouseenter', enlargeItem);
     item.addEventListener('mouseleave', resetItem);
 });
+
+
+// evento de abas
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    // Esconder todo o conteúdo das abas
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remover a classe 'active' de todos os botões
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Mostrar a aba atual e adicionar a classe 'active' ao botão clicado
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Mostrar a primeira aba por padrão ao carregar a página
+document.getElementsByClassName("tablinks")[0].click();
